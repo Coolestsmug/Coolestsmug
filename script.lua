3ad5dee63cfca281a516067576c77818e9adc378
Coolestsmug <161839595+Coolestsmug@users.noreply.github.com>
2024-03-03 11:29:51 +0700

    script.lua

--- script.lua
+++ script.lua
@@ -648,12 +648,173 @@ game:GetService("ReplicatedStorage"):WaitForChild("NetworkEvents"):WaitForChild(
     end)
 end)
 
-zomTab:Toggle("spawn landmine (weak)", function(state)
+zomTab:Toggle("prevent spawnkill (zombie)", function(state)
     getgenv().loopqjh = state
     game:GetService("RunService").Heartbeat:Connect(function()
         if loopqjh == true then
             pcall(function()
-		
+		local args = {
+    [1] = Vector3.new(44.35234451293945, 5.365959167480469, 26.555503845214844),
+    [2] = Vector3.new(23.554460525512695, 1, 37.850868225097656)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ZombieRelated"):WaitForChild("AcidSpit"):FireServer(unpack(args))
+wait()
+local args = {
+    [1] = Vector3.new(-9.678816795349121, 5.362382888793945, 46.75006103515625),
+    [2] = Vector3.new(12.37794303894043, 1, 39.067474365234375)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ZombieRelated"):WaitForChild("AcidSpit"):FireServer(unpack(args))
+wait()
+local args = {
+    [1] = Vector3.new(29.278545379638672, 5.362962245941162, 27.883119583129883),
+    [2] = Vector3.new(19.839088439941406, 1, 30.919002532958984)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ZombieRelated"):WaitForChild("AcidSpit"):FireServer(unpack(args))
+wait()
+local args = {
+    [1] = Vector3.new(18.922393798828125, 5.366894245147705, 58.81745910644531),
+    [2] = Vector3.new(19.588932037353516, 1, 46.68156051635742)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ZombieRelated"):WaitForChild("AcidSpit"):FireServer(unpack(args))
+wait()
+local args = {
+    [1] = Vector3.new(7.932519912719727, 5.349757194519043, 29.92782211303711),
+    [2] = Vector3.new(12.952587127685547, 1, 34.079986572265625)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ZombieRelated"):WaitForChild("AcidSpit"):FireServer(unpack(args))
+wait()
+local args = {
+    [1] = Vector3.new(9.445194244384766, 5.434213161468506, 42.986026763916016),
+    [2] = Vector3.new(8.34402847290039, 1, 49.173927307128906)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ZombieRelated"):WaitForChild("AcidSpit"):FireServer(unpack(args))
+wait()
+local args = {
+    [1] = Vector3.new(12.096213340759277, 46.05311584472656, 42.83441925048828),
+    [2] = Vector3.new(29.159839630126953, 1, 46.822818756103516)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ZombieRelated"):WaitForChild("AcidSpit"):FireServer(unpack(args))
+wait()
+local args = {
+    [1] = Vector3.new(21.971342086791992, 67.66288757324219, 43.97311782836914),
+    [2] = Vector3.new(28.945167541503906, 1, 29.998523712158203)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ZombieRelated"):WaitForChild("AcidSpit"):FireServer(unpack(args))
+wait()
+local args = {
+    [1] = Vector3.new(10.784433364868164, 51.12495803833008, 27.635202407836914),
+    [2] = Vector3.new(9.324787139892578, 1, 22.732925415039062)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ZombieRelated"):WaitForChild("AcidSpit"):FireServer(unpack(args))
+wait()
+local args = {
+    [1] = Vector3.new(10.908175468444824, 35.18443298339844, 26.67213249206543),
+    [2] = Vector3.new(1.5721616744995117, 1, 33.99274444580078)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ZombieRelated"):WaitForChild("AcidSpit"):FireServer(unpack(args))
+wait()
+local args = {
+    [1] = Vector3.new(32.143959045410156, 32.23530578613281, 60.60480499267578),
+    [2] = Vector3.new(15.280675888061523, 1, 55.7652587890625)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ZombieRelated"):WaitForChild("AcidSpit"):FireServer(unpack(args))
+wait()
+local args = {
+    [1] = Vector3.new(34.03712844848633, 28.763200759887695, 26.95634651184082),
+    [2] = Vector3.new(17.477977752685547, 1, 21.750869750976562)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ZombieRelated"):WaitForChild("AcidSpit"):FireServer(unpack(args))
+wait()
+local args = {
+    [1] = Vector3.new(38.76292037963867, 26.908273696899414, 51.250160217285156),
+    [2] = Vector3.new(23.956645965576172, 1, 56.102455139160156)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ZombieRelated"):WaitForChild("AcidSpit"):FireServer(unpack(args))
+wait()
+local args = {
+    [1] = Vector3.new(23.389341354370117, 22.40927505493164, 38.9362678527832),
+    [2] = Vector3.new(-1.4333553314208984, 1, 43.43775939941406)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ZombieRelated"):WaitForChild("AcidSpit"):FireServer(unpack(args))
+wait()
+local args = {
+    [1] = Vector3.new(18.875486373901367, 23.455617904663086, 27.726234436035156),
+    [2] = Vector3.new(2.869516372680664, 1, 26.669734954833984)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ZombieRelated"):WaitForChild("AcidSpit"):FireServer(unpack(args))
+wait()
+local args = {
+    [1] = Vector3.new(32.20348358154297, 30.2653751373291, 50.97477722167969),
+    [2] = Vector3.new(34.01089096069336, 1, 38.1528205871582)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ZombieRelated"):WaitForChild("AcidSpit"):FireServer(unpack(args))
+wait()
+local args = {
+    [1] = Vector3.new(32.004356384277344, 23.961029052734375, 31.314598083496094),
+    [2] = Vector3.new(26.61473846435547, 1, 21.144412994384766)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ZombieRelated"):WaitForChild("AcidSpit"):FireServer(unpack(args))
+wait()
+local args = {
+    [1] = Vector3.new(39.88054656982422, 38.73746871948242, 35.30915832519531),
+    [2] = Vector3.new(34.94029235839844, 1, 25.787796020507812)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ZombieRelated"):WaitForChild("AcidSpit"):FireServer(unpack(args))
+wait()
+local args = {
+    [1] = Vector3.new(35.952850341796875, 41.29209518432617, 41.73908233642578),
+    [2] = Vector3.new(39.80612564086914, 1, 31.96713638305664)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ZombieRelated"):WaitForChild("AcidSpit"):FireServer(unpack(args))
+wait()
+local args = {
+    [1] = Vector3.new(28.336002349853516, 36.502498626708984, 55.610130310058594),
+    [2] = Vector3.new(37.89435577392578, 1, 45.20414352416992)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ZombieRelated"):WaitForChild("AcidSpit"):FireServer(unpack(args))
+wait()
+local args = {
+    [1] = Vector3.new(28.84077262878418, 41.29795455932617, 60.43194580078125),
+    [2] = Vector3.new(32.001529693603516, 1, 52.4228401184082)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ZombieRelated"):WaitForChild("AcidSpit"):FireServer(unpack(args))
+wait()
+local args = {
+    [1] = Vector3.new(3.665933132171631, 30.512939453125, 43.34560012817383),
+    [2] = Vector3.new(5.261941909790039, 1, 41.78221130371094)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ZombieRelated"):WaitForChild("AcidSpit"):FireServer(unpack(args))
+wait()
+local args = {
+    [1] = Vector3.new(14.185626983642578, 15.68039608001709, 64.00680541992188),
+    [2] = Vector3.new(6.018622398376465, 1, 57.25159454345703)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ZombieRelated"):WaitForChild("AcidSpit"):FireServer(unpack(args))
+wait()
+								
             end)
         end
     end)
@@ -664,6 +825,7 @@ zomTab:Toggle("anti human escape", function(state)
     game:GetService("RunService").Heartbeat:Connect(function()
         if lopoqjh == true then
             pcall(function()
+		wait(5)
 		local args = {
     [1] = Vector3.new(-111.21009063720703, 48.495262145996094, 43.27946472167969),
     [2] = Vector3.new(-105.5, 42.27115249633789, 48.14269256591797)
