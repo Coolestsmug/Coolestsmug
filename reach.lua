c71ff25fbf79fd00eb86c46c7a9819e6408b3c85
Coolestsmug <161839595+Coolestsmug@users.noreply.github.com>
2024-03-09 20:15:58 +0700

    Update reach.lua

--- reach.lua
+++ reach.lua
@@ -3,7 +3,7 @@ local trueActive = true
 local reachType = "Sphere"
 local dmgEnabled = true
 local visualizerEnabled = false
- 
+
 local visualizer = Instance.new("Part")
 visualizer.BrickColor = BrickColor.Blue()
 visualizer.Transparency = 0.6
@@ -12,7 +12,7 @@ visualizer.CanCollide = false
 visualizer.Size = Vector3.new(0.5,0.5,0.5)
 visualizer.BottomSurface = Enum.SurfaceType.Smooth
 visualizer.TopSurface = Enum.SurfaceType.Smooth
- 
+
 local ScreenGui = Instance.new("ScreenGui")
 local Frame = Instance.new("Frame")
 local TextLabel = Instance.new("TextLabel")
@@ -26,13 +26,13 @@ local Frame_3 = Instance.new("Frame")
 local Frame_4 = Instance.new("Frame")
 local Frame_5 = Instance.new("Frame")
 local TextButton_2 = Instance.new("TextButton")
- 
+
 --Properties:
- 
+
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 ScreenGui.DisplayOrder = 999999999
 ScreenGui.ResetOnSpawn = false
- 
+
 Frame.Parent = ScreenGui
 Frame.AnchorPoint = Vector2.new(0, 0.5)
 Frame.BackgroundColor3 = Color3.fromRGB(45, 45, 45)
@@ -41,7 +41,7 @@ Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
 Frame.BorderSizePixel = 4
 Frame.Position = UDim2.new(0, 0, 0.600000024, 0)
 Frame.Size = UDim2.new(0.150000006, 0, 0.300000012, 0)
- 
+
 TextLabel.Parent = Frame
 TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.BackgroundTransparency = 1.000
@@ -52,7 +52,7 @@ TextLabel.TextColor3 = Color3.fromRGB(255,255, 255)
 TextLabel.TextScaled = true
 TextLabel.TextSize = 14.000
 TextLabel.TextWrapped = true
- 
+
 TextBox.Parent = Frame
 TextBox.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextBox.BackgroundTransparency = 1.000
@@ -64,7 +64,7 @@ TextBox.TextColor3 = Color3.fromRGB(0,0,255)
 TextBox.TextScaled = true
 TextBox.TextSize = 14.000
 TextBox.TextWrapped = true
- 
+
 TextLabel_2.Parent = Frame
 TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel_2.BackgroundTransparency = 1.000
@@ -76,7 +76,7 @@ TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel_2.TextScaled = true
 TextLabel_2.TextSize = 14.000
 TextLabel_2.TextWrapped = true
- 
+
 TextLabel_3.Parent = Frame
 TextLabel_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel_3.BackgroundTransparency = 1.000
@@ -88,7 +88,7 @@ TextLabel_3.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel_3.TextScaled = true
 TextLabel_3.TextSize = 14.000
 TextLabel_3.TextWrapped = true
- 
+
 TextLabel_4.Parent = Frame
 TextLabel_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel_4.BackgroundTransparency = 1.000
@@ -100,7 +100,7 @@ TextLabel_4.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel_4.TextScaled = true
 TextLabel_4.TextSize = 14.000
 TextLabel_4.TextWrapped = true
- 
+
 TextButton.Parent = Frame
 TextButton.AnchorPoint = Vector2.new(0, 1)
 TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
@@ -113,7 +113,7 @@ TextButton.TextColor3 = Color3.fromRGB(0, 0, 255)
 TextButton.TextScaled = true
 TextButton.TextSize = 14.000
 TextButton.TextWrapped = true
- 
+
 Frame_2.Parent = Frame
 Frame_2.Active = true
 Frame_2.AnchorPoint = Vector2.new(0, 0.5)
@@ -122,14 +122,14 @@ Frame_2.BorderSizePixel = 0
 Frame_2.Position = UDim2.new(0.725000024, 0, 0.699999988, 0)
 Frame_2.Size = UDim2.new(0, 25, 0, 25)
 Frame_2.ZIndex = 5
- 
+
 Frame_3.Parent = Frame_2
 Frame_3.AnchorPoint = Vector2.new(0.5, 0.5)
 Frame_3.BackgroundColor3 = Color3.fromRGB(0,0,255)
 Frame_3.BorderSizePixel = 0
 Frame_3.LayoutOrder = 1
 Frame_3.Position = UDim2.new(0.5, 0, 0.5, 0)
- 
+
 Frame_4.Parent = Frame
 Frame_4.Active = true
 Frame_4.AnchorPoint = Vector2.new(0, 0.5)
@@ -138,7 +138,7 @@ Frame_4.BorderSizePixel = 0
 Frame_4.Position = UDim2.new(0.725000024, 0, 0.5, 0)
 Frame_4.Size = UDim2.new(0, 25, 0, 25)
 Frame_4.ZIndex = 5
- 
+
 Frame_5.Parent = Frame_4
 Frame_5.AnchorPoint = Vector2.new(0.5, 0.5)
 Frame_5.BackgroundColor3 = Color3.fromRGB(0,0,255)
@@ -146,7 +146,7 @@ Frame_5.BorderSizePixel = 0
 Frame_5.LayoutOrder = 1
 Frame_5.Position = UDim2.new(0.5, 0, 0.5, 0)
 Frame_5.Size = UDim2.new(1, 0, 1, 0)
- 
+
 TextButton_2.Parent = Frame
 TextButton_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextButton_2.BackgroundTransparency = 1.000
@@ -158,12 +158,12 @@ TextButton_2.TextColor3 = Color3.fromRGB(0,0,255)
 TextButton_2.TextScaled = true
 TextButton_2.TextSize = 14.000
 TextButton_2.TextWrapped = true
- 
+
 repeat wait() until game.Players.LocalPlayer
 ScreenGui.Parent = game:GetService("CoreGui")
- 
- 
- 
+
+
+
 Frame_4.InputBegan:connect(function(inp)
 	if inp.UserInputType == Enum.UserInputType.MouseButton1 then
 		dmgEnabled = not dmgEnabled
@@ -202,9 +202,9 @@ game:GetService("UserInputService").InputBegan:connect(function(inp,gpe)
 		ScreenGui.Enabled = not ScreenGui.Enabled
 	end
 end)
- 
+
 local plr = game.Players.LocalPlayer
- 
+
 local function onHit(hit,handle)
     local victim = hit.Parent:FindFirstChildOfClass("Humanoid")
 	    if victim and victim.Parent.Name ~= game.Players.LocalPlayer.Name then
@@ -221,7 +221,7 @@ local function onHit(hit,handle)
 		end
     end
 end
- 
+
 local function getWhiteList()
     local wl = {}
     for _,v in pairs(game.Players:GetPlayers()) do
@@ -238,7 +238,7 @@ local function getWhiteList()
     end
     return wl
 end
- 
+
 game:GetService("RunService").RenderStepped:connect(function()
     if not active or not trueActive then return end
 	local s = plr.Character and plr.Character:FindFirstChildOfClass("Tool")
