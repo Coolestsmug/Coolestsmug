d998b6b92ebe216c1c563b875a0df8c306159fd5
Coolestsmug <161839595+Coolestsmug@users.noreply.github.com>
2024-03-12 09:59:47 +0700

    Update and rename Isolation_warm.lua to Isolationwarm.lua

--- Isolationwarm.lua
+++ Isolationwarm.lua
@@ -1,4 +1,31 @@
-local isoTab = Window:Tab("warm isolation","rbxassetid://12308581351")
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/UI-Library/main/Source/MyUILib(Unamed).lua"))();
+local Window = Library:Create("Hitbox Expander")
+
+local ToggleGui = Instance.new("ScreenGui")
+local Toggle = Instance.new("TextButton")
+
+ToggleGui.Name = "ToggleGui_HE"
+ToggleGui.Parent = game.CoreGui
+
+Toggle.Name = "tog"
+Toggle.Parent = ToggleGui
+Toggle.BackgroundColor3 = Color3.fromRGB(24, 24, 24)
+Toggle.BackgroundTransparency = 0.8
+Toggle.Position = UDim2.new(0, 0, 0.454706937, 0)
+Toggle.Size = UDim2.new(0.04, 0, 0.0650164187, 0)
+Toggle.Font = Enum.Font.SourceSans
+Toggle.Text = "Toggle"
+Toggle.TextScaled = true
+Toggle.TextColor3 = Color3.fromRGB(40, 40, 40)
+Toggle.TextSize = 24.000
+Toggle.TextXAlignment = Enum.TextXAlignment.Left
+Toggle.Active = true
+Toggle.Draggable = true
+Toggle.MouseButton1Click:connect(function()
+    Library:ToggleUI()
+end)
+
+local isoTab = Window:Tab("vote cast","rbxassetid://12308581351")
 isoTab:Section("vote submit")
 isoTab:InfoLabel("stacked, not for you")
 isoTab:Button("yes", function()
@@ -48,7 +75,7 @@ game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Shop"
     end)
 end)
 
-isoTab:Section("misc")
+local isoTab = Window:Tab("misc","rbxassetid://12308581351")
 isoTab:Button("remove IFP", function()
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Hallway"):WaitForChild("InFirstPerson"):Destroy()
 end)
