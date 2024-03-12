38828a73eade637588c640f42fe8b9ec6a607c43
Coolestsmug <161839595+Coolestsmug@users.noreply.github.com>
2024-03-12 09:54:49 +0700

    Update zombiegame.lua

--- zombiegame.lua
+++ zombiegame.lua
@@ -1,6 +1,30 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/UI-Library/main/Source/MyUILib(Unamed).lua"))();
 local Window = Library:Create("Hitbox Expander")
 
+local ToggleGui = Instance.new("ScreenGui")
+local Toggle = Instance.new("TextButton")
+
+ToggleGui.Name = "ToggleGui_HE"
+ToggleGui.Parent = game.CoreGui
+
+Toggle.Name = "tog"
+Toggle.Parent = ToggleGui
+Toggle.BackgroundColor3 = Color3.fromRGB(24, 24, 24)
+Toggle.BackgroundTransparency = 0.8
+Toggle.Position = UDim2.new(0, 0, 0.454706937, 0)
+Toggle.Size = UDim2.new(0.04, 0, 0.0650164187, 0)
+Toggle.Font = Enum.Font.SourceSans
+Toggle.Text = "Toggle"
+Toggle.TextScaled = true
+Toggle.TextColor3 = Color3.fromRGB(40, 40, 40)
+Toggle.TextSize = 24.000
+Toggle.TextXAlignment = Enum.TextXAlignment.Left
+Toggle.Active = true
+Toggle.Draggable = true
+Toggle.MouseButton1Click:connect(function()
+    Library:ToggleUI()
+end)
+
 local zomTab = Window:Tab("game", "rbxassetid://12308581351")
 
 local zomTab = Window:Tab("zombie","rbxassetid://12308581351")
