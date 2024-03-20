caec813675dd102910771f8f39641f86eb5f7910
Coolestsmug <161839595+Coolestsmug@users.noreply.github.com>
2024-03-20 20:39:00 +0700

    Create Anim2.lua

---
+++ Anim2.lua
@@ -0,0 +1,56 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/UI-Library/main/Source/MyUILib(Unamed).lua"))();
+local Window = Library:Create("animation changer")
+
+local ToggleGui = Instance.new("ScreenGui")
+local Toggle = Instance.new("TextButton")
+
+ToggleGui.Name = "ToggleGui_HE"
+ToggleGui.Parent = game.CoreGui
+
+Toggle.Name = "tog"
+Toggle.Parent = ToggleGui
+Toggle.BackgroundColor3 = Color3.fromRGB(24, 24, 24)
+Toggle.BackgroundTransparency = 0.8
+Toggle.Position = UDim2.new(0, 0, 0.454706937, 0)
+Toggle.Size = UDim2.new(0.04, 0, 0.0650164187, 0)
+Toggle.Font = Enum.Font.SourceSans
+Toggle.Text = "Toggle"
+Toggle.TextScaled = true
+Toggle.TextColor3 = Color3.fromRGB(40, 40, 40)
+Toggle.TextSize = 24.000
+Toggle.TextXAlignment = Enum.TextXAlignment.Left
+Toggle.Active = true
+Toggle.Draggable = true
+Toggle.MouseButton1Click:connect(function()
+    Library:ToggleUI()
+end)
+
+local sTab = Window:Tab("Bold (gay)","rbxassetid://10888331510")
+
+sTab:Button("", function()
+
+end)
+
+sTab:Button("", function()
+
+end)
+
+sTab:Button("", function()
+
+end)
+
+sTab:Button("", function()
+
+end)
+
+sTab:Button("", function()
+
+end)
+
+sTab:Button("", function()
+
+end)
+
+sTab:Button("", function()
+
+end)
