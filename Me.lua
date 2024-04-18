5f8c72c14db62664061f97d51b8664415691430b
Coolestsmug <161839595+Coolestsmug@users.noreply.github.com>
2024-04-18 12:02:15 +0700

    Update Me.lua

--- Me.lua
+++ Me.lua
@@ -286,7 +286,7 @@ gagTab:Button("boobs n ass 🗣️ (perm)", function()
 (getgenv and getgenv() or _G or shared).BB_Settings = {
 Breast = true,
 BreastPhysics = { Target = 5, Speed = 10, Damper = 0.2 },
-BreastSize = 1,
+BreastSize = 0.5,
 Cheeks = true,
 CheeksPhysics = { Target = 3, Speed = 10, Damper = 0.1 },
 CheeksSize = 1,
@@ -296,6 +296,10 @@ Only_Others = false,
 Simulate_Physics_On_LocalPlayer_Only = false,
 }
 -- для инжектора Key: 7ecde6e1148745b0a8bf0d9aad691beb
+--[[
+  true-on
+  false-off
+]]
 loadstring(
 game:HttpGet("https://gist.githubusercontent.com/htt-py/92db22eeefad0042a6da9117501ad827/raw/BBies.luau", true)
 )()
