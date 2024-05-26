0298d79ca4324ad504a557aa78b4371900030fe2
Coolestsmug <161839595+Coolestsmug@users.noreply.github.com>
2024-05-26 10:46:42 +0700

    Update Isolationwarm.lua

--- Isolationwarm.lua
+++ Isolationwarm.lua
@@ -46,33 +46,31 @@ end)
 isoTab:Section("vote loop")
 isoTab:InfoLabel("stacked, not for you")
 isoTab:Toggle("yes", function(state)
-    getgenv().lojophhk = state
-    game:GetService("RunService").Heartbeat:Connect(function()
-        if lojophhk == true then
+    getgenv().votecast1 = state
+end)
+    task.wait(0.5)
+        if votecast1 == true then
             pcall(function()
                     local args = {
     [1] = true
 }
-
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Shop"):WaitForChild("SubmitVote"):FireServer(unpack(args))
-            end)
-        end
-    end)
+        end)
+    end
 end)
 
 isoTab:Toggle("no", function(state)
-    getgenv().lojopkhhk = state
-    game:GetService("RunService").Heartbeat:Connect(function()
-        if lojopkhhk == true then
+    getgenv().votecast2 = state
+end)
+    task.wait(0.5)
+        if votecast2 == true then
             pcall(function()
                     local args = {
     [1] = false
 }
-
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Shop"):WaitForChild("SubmitVote"):FireServer(unpack(args))
-            end)
-        end
-    end)
+        end)
+    end
 end)
 
 local isoTab = Window:Tab("misc","rbxassetid://12308581351")
@@ -82,11 +80,11 @@ end)
 
 isoTab:Toggle("spray", function(state)
     getgenv().lojoplkhhk = state
-    game:GetService("RunService").Heartbeat:Connect(function()
-        if lojoplkhhk == true then
-            pcall(function()
-                    game:GetService("Players").LocalPlayer.Character:FindFirstChild("Spray bottle").StartSpray:FireServer()
-            end)
-        end
-    end)
+end)
+task.wait(0.2)
+    if lojoplkhhk == true then
+        pcall(function()
+                game:GetService("Players").LocalPlayer.Character:FindFirstChild("Spray bottle").StartSpray:FireServer()
+        end)
+    end
 end)
